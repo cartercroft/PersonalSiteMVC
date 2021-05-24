f91aecd3b5247b302feab59bb5ed4f95c0c250d7
cartercroft <carterncroft@gmail.com>
2021-05-24 17:31:02 -0500

    Formatted projects page, added links to each project and github

--- PersonalSiteMVC/PersonalSiteMVC.UI/Controllers/HomeController.cs
+++ PersonalSiteMVC/PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -13,17 +13,23 @@ namespace PersonalSiteMVC.UI.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
+        public ActionResult Links()
+        { 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
             return View();
         }
     }
