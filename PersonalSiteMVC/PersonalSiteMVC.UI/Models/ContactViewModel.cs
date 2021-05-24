36759926d53bbf29e5d696a983b384ac94e79223
cartercroft <carterncroft@gmail.com>
2021-05-24 17:35:29 -0500

    Added ContactViewModel

---
+++ PersonalSiteMVC/PersonalSiteMVC.UI/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalSiteMVC.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "*Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is required.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required.")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
